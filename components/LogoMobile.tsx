9059a9c088f3e5e7ade94d02903d37882b1704b9
NamNV57 <nam.nguyenbku00@gmail.com>
2024-06-18 17:02:28 +0700

    nam commit

---
+++ components/LogoMobile.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LogoMobile = () => {
+    return (
+    <a href="/" className="flex items-center gap-2">
+        <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        BudgetTracker
+        </p>
+    </a>
+    );
+}
+
+export default LogoMobile
---
+++ components/Navbar/MobileNavbar.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
+import Logo from '../Logo';
+import { items } from '@/lib/Item';
+import NavbarItem from './NavbarItem';
+import LogoMobile from '../LogoMobile';
+import ThemeSwitcherBtn from './ThemeSwitcherBtn';
+
+const MobileNavbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+      <div className="block border-separate bg-background md:hidden">
+        <nav className="container flex items-center justify-between px-8">
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <Button variant={"ghost"} size={"icon"}>
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+              <Logo />
+              <div className="flex flex-col gap-1 pt-4">
+                {items.map((item) => (
+                  <NavbarItem
+                    key={item.label}
+                    link={item.link}
+                    label={item.label}
+                    clickCallback={() => setIsOpen((prev) => !prev)}
+                  />
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+          <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+            <LogoMobile />
+          </div>
+          <div className="flex items-center gap-2">
+            <ThemeSwitcherBtn />
+          </div>
+        </nav>
+      </div>
+    );
+}
+
+export default MobileNavbar
